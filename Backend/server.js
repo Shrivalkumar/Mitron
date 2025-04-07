92df5955353a0e6962458bb90fbb24f74410e0e3
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2025-04-08 01:09:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv';
 import connectMongoD from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -9,6 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(express.json()); //using middleware to parse json data between req and res (to parse req.body())
+app.use(express.urlencoded({ extended: true })); //using middleware to parse url encoded data between req and res (to parse req.body())
+
+app.use(cookieParser()); //using middleware to parse cookies 
 
 app.use("/api/auth" , authRoutes);
 
