8a19bcbdc54d5abd9566ce1c37f7060744c2b079
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2025-04-09 14:32:15 +0530

    added likedPosts array in userSchema

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,57 +1,66 @@
 import mongoose from "mongoose";
 
-
-const userSchema = new mongoose.Schema({ //creating a user schema 
-    username:{
-        type:String,
-        required:true,
-        unique: true
+const userSchema = new mongoose.Schema(
+  {
+    //creating a user schema
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    fullName:{
-        type:String,
-        required:true
+    fullName: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
-        minLength: 8
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    }, 
-    followers:[{
-        type:mongoose.Schema.Types.ObjectId, // followers will be an array of user ids 
-        ref:"User", // referencing the user model
-        default: [] // default value is an empty array
-    }],
-    following: [{
-        type:mongoose.Schema.Types.ObjectId, // following will be an array of user ids
-        ref:"User", // referencing the user model
-        default: []
-    }],
-    profileImg:{
-        type:String,
-        default:""
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    coverImg:{
-        type:String,
-        default:""
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // followers will be an array of user ids
+        ref: "User", // referencing the user model
+        default: [], // default value is an empty array
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId, // following will be an array of user ids
+        ref: "User", // referencing the user model
+        default: [],
+      },
+    ],
+    profileImg: {
+      type: String,
+      default: "",
     },
-    bio:{
-        type:String,
-        default:""
+    coverImg: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
     },
     link: {
-        type:String,
-        default:""
-        }
+      type: String,
+      default: "",
+    },
+    likedPosts: [{
+        type: mongoose.Schema.Types.ObjectId, // likedPosts will be an array of post ids
+        ref: "Post", 
+        default: [],
+    }]
+  },
+  { timestamps: true }
+); // this gives us all the timeStamps as updated and created
 
-
-} , {timestamps: true});// this gives us all the timeStamps as updated and created 
-
-
-const User = mongoose.model("User", userSchema); // creating a userModel with a userSchema we just created 
+const User = mongoose.model("User", userSchema); // creating a userModel with a userSchema we just created
 
 export default User;
